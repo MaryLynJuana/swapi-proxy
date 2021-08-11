f1906b94812c89f2712381b0cde79fb37b38208b
MaryLynJuana <mary_juana@ukr.net>
2021-08-11 16:49:30 +0300

    refactor(api): Move callback from handlers to avoid duplicate code

--- src/server.js
+++ src/server.js
@@ -2,58 +2,47 @@
 
 const http = require('http');
 const https = require('https');
-const { parse } = require('url');
+const { URL } = require('url');
 const encodings = require('./encodings.js');
 
 const swapiUrl = 'https://swapi.dev/api';
 
 const routing = {
   'GET': {
-    '/people': (id, encoding, res) => (
-      https.get(`${swapiUrl}/people/${id}`, response => {
-        const { statusCode } = response;
-        res.writeHead(statusCode);
-        if (statusCode !== 200) {
-          return res.end(`Request failed. Got status code: ${statusCode}`);
-        }
-        let data = '';
-        response.on('data', (chunk) => { data += chunk; });
-        response.on('end', () => {
-          const encodeFun = encodings[encoding];
-          if (encodeFun) data = encodeFun(data);
-          res.end(data);
-        });
-      })
-    ),
-    '/planets': (id, encoding, res) => (
-      https.get(`${swapiUrl}/planets/${id}`, response => {
-        const { statusCode } = response;
-        res.writeHead(statusCode);
-        if (statusCode !== 200) {
-          return res.end(`Request failed. Got status code: ${statusCode}`);
-        }
-        let data = '';
-        response.on('data', (chunk) => { data += chunk; });
-        response.on('end', () => {
-          const encodeFun = encodings[encoding];
-          if (encodeFun) data = encodeFun(data);
-          res.end(data);
-        });
-      })
-    ),
+    '/people': (id, searchPars, callback) => {
+      console.log(`Sending request to swapi for route /people/${id}`);
+      https.get(`${swapiUrl}/people/${id}${searchPars}`, callback);
+    },
+    '/planets': (id, searchPars, callback) => {
+      console.log(`Sending request to swapi for route /planets/${id}`);
+      https.get(`${swapiUrl}/planets/${id}${searchPars}`, callback)
+    },
   },
 };
 
 module.exports = http.createServer((req, res) => {
-  const { url } = req;
-  const urlParts = url.split('/');
+  const url = new URL(req.url, 'http://' + req.headers.host);
+  const urlParts = url.pathname.split('/');
   const id = parseInt(urlParts[urlParts.length - 1]);
   if (!isNaN(id)) urlParts.pop();
-  const { encoding } = parse(req.url,true).query;
   const handler = routing[req.method][urlParts.join('/')];
   if (!handler) {
     res.writeHead(404);
     return res.end('Page not found :(');
   }
-  handler((isNaN(id) ? '' : id), encoding, res);
+  const callback = response => {
+    const { statusCode } = response;
+    res.writeHead(statusCode);
+    if (statusCode !== 200) {
+      return res.end(`Request failed. Got status code: ${statusCode}`);
+    }
+    let data = '';
+    response.on('data', (chunk) => { data += chunk; });
+    response.on('end', () => {
+      const encodeFun = encodings[url.searchParams.get('encoding')];
+      if (encodeFun) data = encodeFun(data);
+      res.end(data);
+    });
+  };
+  handler((isNaN(id) ? '' : id), url.search, callback);
 });
